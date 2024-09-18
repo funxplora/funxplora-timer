8a8a96ad59cd6dd7395065ca24488a4b533b7fd4
anand <vermaanand278@gmail.com>
2024-09-18 22:52:44 +0530

    saf

--- index.js
+++ index.js
@@ -75,10 +75,101 @@ if (trx) {
     trx = false;
   }, delay);
 }
+
+const generatedTimeEveryAfterEveryFiveMinTRXJackPod = () => {
+  let min = 4;
+  let sec = 60;
+  let jackpodTrxJob = schedule.schedule("* * * * * *", async function () {
+    // let interval = setInterval(async () => {
+    const currentTime = sec;
+    sec = sec - 1;
+    if (sec < 0) {
+      sec = 59;
+    }
+    const timeToSend = sec;
+    io.emit("fivemintrxjackpod", `${min}_${timeToSend}`);
+    if (min === 0 && timeToSend === 6) {
+      const datetoAPISend = parseInt(new Date().getTime().toString());
+      const actualtome = soment.tz("Asia/Kolkata");
+      const time = actualtome.add(5, "hours").add(30, "minutes").valueOf();
+
+      //////////////////// get transaction id /////////////////////
+      await getTransactionidForJackPod([])
+        .then(async (result) => {
+          //////////////////// get manual result ///////////////////
+          const trans_id = result?.[0]?.tr_tranaction_id;
+          const get_result_for_transid = `SELECT result FROM trx_manual_result where tr_type = ? AND trans_id = ?;`;
+          await queryDb(get_result_for_transid, [
+            4,
+            String(Number(trans_id) + 1),
+          ])
+            .then((result) => {
+              const result_arrray = [2002, 1000, 3002, 3001, 2001, 2005, 2004];
+              let result_number =
+                result?.[0]?.result ||
+                result_arrray[Math.floor(Math.random() * result_arrray.length)];
+
+              setTimeout(async () => {
+                const query = `CALL sp_insert_trx_five_min_jackpod_result(?, ?, ?, ?, ?, ?, ?)`;
+                await queryDb(query, [
+                  result_number,
+                  String(moment(time).format("HH:mm:ss")),
+                  4,
+                  `**${randomStr(4, "0123456789abcdefghijklmnopqrstuvwxyz")}`,
+                  JSON.stringify(
+                    randomStr(10, "0123456789abcdefghijklmnopqrstuvwxyz")
+                  ),
+                  `${randomStr(
+                    10,
+                    "0123456789abcdefghijklmnopqrstuvwxyz"
+                  )?.slice(-5)}`,
+                  randomStr(8, "0123456789"),
+                ])
+                  .then((result) => {})
+                  .catch((e) => {
+                    console.log(e);
+                  });
+
+                ////////////////////// clear bet ////////////
+                await jackPodClearBet([String(result_number)])
+                  .then((result) => {})
+                  .catch((e) => {
+                    console.log(e);
+                  });
+                //////////// update mediator table //////////////////////
+                await updateMediatorTableJackPod([0, 0])
+                  .then((result) => {})
+                  .catch((e) => {
+                    console.log(e);
+                  });
+              }, [1000]);
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+    if (currentTime === 0) {
+      min--;
+      if (min < 0) {
+        clearInterval(interval);
+        jackpodTrxJob?.cancel();
+        jackpodTrxJob?.cancel();
+        jackpodTrxJob?.cancel();
+        min = 0;
+        sec = 0;
+      } // Reset min to 4 when it reaches 0
+    }
+    // }, 1000);
+  });
+};
 const jackpodResult = async (req, res) => {
   setTimeout(() => {
     try {
-      allroutes.generatedTimeEveryAfterEveryFiveMinTRXJackPod(io);
+      generatedTimeEveryAfterEveryFiveMinTRXJackPod();
       return res.status(200)?.json({
         msg: "APi hit successfully",
       });
